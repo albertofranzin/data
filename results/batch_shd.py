d7c09591ee1d9ea922e3a56b4874eea42a9532ea
Alberto Franzin <alberto.franzin@gmail.com>
2014-03-27 10:52:02 +0100

    some other results

---
+++ results/batch_shd.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python -w
+
+import subprocess
+import sys
+import re
+
+instance_name  = sys.argv[1]
+dir_to_compare = sys.argv[2]
+
+ls_command    = "ls "+dir_to_compare+"/test.job.o*"
+list_of_files = subprocess.Popen(ls_command, stdout=subprocess.PIPE, shell=True)
+(output, err) = list_of_files.communicate()
+output        = output.rstrip().split('\n')
+
+for f in output:
+    with open(f, "r") as resfile:
+        lines = resfile.readlines()
+        found = False
+        for line in lines:
+            mat = re.match("net:", line)
+            if mat != None:
+                found = True
+                lis = line.rstrip('\n').strip(' ').strip('\t').split(":")
+                #print lis
+                factors = lis[1].replace('[','(').replace(']',')')
+                #print factors
+                r_command = "Rscript compute.shd.R 2 "+instance_name+".mat \""+factors+"\""
+                #print r_command
+                r_out     =  subprocess.Popen(r_command, stdout=subprocess.PIPE, shell=True)
+                (o2, e2)  = r_out.communicate()
+                o         = o2.rstrip().split('\n')
+                for rline in o:
+                    #print rline
+                    if re.match("SHD:",rline) != None:
+                        print rline.rstrip('\n').strip(' ').strip('\t').split(':')[1]
+        if found == False:
+            print "-"
+
---
+++ results/compute.shd.R
@@ -0,0 +1,20 @@
+library("bnstruct")
+
+# usage: Rscript compute.shd.R <mode> <mat.1> <mat.2>
+# mode: 1 to compare 2 adjacency matrices contained in two files
+#       2 to compare two matrices, the first in a file, the second as string of factors
+# mat.1: adjacency matrix file
+# mat.2: adjacency matrix file or string, depending on mode parameter
+
+args         <- commandArgs(trailingOnly = TRUE)
+mode         <- args[1]
+input.file.1 <- args[2]
+a <- read.table(input.file.1, header=FALSE, sep=" ")
+if (mode == 1) {
+  input.file.2 <- args[3]
+  b <- read.table(input.file.2, header=FALSE, sep=" ")
+} else {
+  b <- factors.to.graph(args[3])
+}
+
+cat("SHD: ", shd(a,b),"\n")
